ef62ecb05a549630a3b1c911084a15baec054db6
Kohmmz <jeremiahkoome1@gmail.com>
2025-02-16 12:14:54 +0300

    implement return_evens function to filter even numbers from a list

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_number =[num_list for num_list in num_list if num_list % 2 == 0]
+    return even_number
 
+
+print(return_evens([1,2,3,4,5,6,7,8]))
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
